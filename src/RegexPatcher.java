6720d0fd137857a6615f052a9b64e5a07d886994
TOXWORD <yan.kalinin@mail.ru>
2019-10-09 21:38:44 +0300

    Add files via upload

---
+++ src/RegexPatcher.java
@@ -0,0 +1,29 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexPatcher {
+
+
+//    String s = "1233333";
+//
+//    Pattern p = Pattern.compile("[0-9]{2}");
+//    Matcher m = p.matcher(s);
+//
+//        if(m.find())
+//                System.out.println(m.group());
+//
+//    String t = "(1231231))23123) 123123123123 123213213123";
+//
+//        System.out.println(t.replaceAll("\\([^)]*\\)", ""));
+
+    RegexPatcher(){
+
+    }
+
+    public void enterText(){
+
+
+
+    }
+
+}
