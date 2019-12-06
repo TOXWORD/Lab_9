7f799f23703c2e92fc997b8b5f628ef2613be489
TOXWORD <yan.kalinin@mail.ru>
2019-12-06 23:25:42 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            RegexPatcher rp = new RegexPatcher();
+            rp.removeBrackets();
+            rp.removeNumbers();
+            rp.removeZero();
+        } catch (FileNotFoundException e) {
+            System.out.println("FNF");
+        } catch (IOException e) {
+            System.out.println("IOE");
+        }
     }
 }
--- src/RegexPatcher.java
+++ src/RegexPatcher.java
@@ -1,29 +1,101 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexPatcher {
 
+    private List<String> text;
 
-//    String s = "1233333";
-//
-//    Pattern p = Pattern.compile("[0-9]{2}");
-//    Matcher m = p.matcher(s);
-//
-//        if(m.find())
-//                System.out.println(m.group());
-//
-//    String t = "(1231231))23123) 123123123123 123213213123";
-//
-//        System.out.println(t.replaceAll("\\([^)]*\\)", ""));
-
-    RegexPatcher(){
-
+    RegexPatcher() throws FileNotFoundException {
+        text = new ArrayList<>();
+        enterText();
     }
 
-    public void enterText(){
-
-
-
+    public void enterText() throws FileNotFoundException {
+        File f = new File("input.txt");
+        Scanner sc = new Scanner(f);
+        while (sc.hasNextLine()) {
+            text.add(sc.nextLine());
+        }
     }
 
+    public void removeBrackets() throws IOException {
+        Pattern p = Pattern.compile("\\([^()]*\\)");
+        Matcher m;
+        try (FileWriter fw = new FileWriter("output.txt")) {
+            for (String row : text) {
+                String s = row.replaceAll(p.pattern(), "");
+                m = p.matcher(s);
+                while (m.find()) {
+                    s = m.replaceAll("");
+                    m = p.matcher(s);
+                }
+                s = s.replaceAll("[()]*", "");
+                s = s.replaceAll("^\\s", "");
+                fw.write(s + "\n");
+            }
+        }
+    }
+
+    public void removeNumbers() throws IOException {
+        Pattern p = Pattern.compile("[0-9]{2}|[0-9]");
+        Matcher m;
+        StringTokenizer st;
+        String temp;
+        try (FileWriter fw = new FileWriter("output1.txt")) {
+            for (String row : text) {
+                st = new StringTokenizer(row, findDelims(row, '0', '9'), true);
+                while (st.hasMoreTokens()) {
+                    temp = st.nextToken();
+                    m = p.matcher(temp);
+                    if (m.find()) {
+                        fw.write(m.group());
+                    }
+                    else {
+                        fw.write(temp);
+                    }
+                }
+                fw.write("\n");
+            }
+        }
+    }
+
+    public String findDelims(String word, char exceptFrom, char exceptTo) {
+        StringBuilder delims = new StringBuilder();
+        int rowLen = word.length();
+        for (int i = 0; i < rowLen; i++) {
+            if (!(word.charAt(i) >= exceptFrom && word.charAt(i) <= exceptTo)) {
+                delims.append(word.charAt(i));
+            }
+        }
+        return delims.toString();
+    }
+
+    public void removeZero() throws IOException {
+        try (FileWriter fw = new FileWriter("output2.txt")) {
+            Pattern p = Pattern.compile("^0+");
+            Matcher m;
+            StringTokenizer st;
+            for (String row : text) {
+                st = new StringTokenizer(row, findDelims(row, '0', '9'), true);
+                while (st.hasMoreTokens()) {
+                    m = p.matcher(st.nextToken());
+                    if(m.matches()){
+                        fw.write(m.replaceAll("0"));
+                    }
+                    else{
+                        fw.write(m.replaceAll(""));
+                    }
+                }
+                fw.write("\n");
+            }
+        }
+    }
 }
